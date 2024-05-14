75f98945ddb865401d51f2e27e16b61fdaafcd6b
MatthewSingler <matthew.singler@gmail.com>
2024-05-14 13:13:31 -0500

    Went through text values

--- tests/userLocators.spec.ts
+++ tests/userLocators.spec.ts
@@ -79,3 +79,25 @@ test('reusable locators', async ({page}) => {
 
     await expect (emailField).toHaveValue('test@test.com')
 })
+
+test('extracting values', async ({page}) => {
+    //single text value
+    const basicForm = page.locator('nb-card').filter({hasText: "Basic Form"})
+
+    const buttonText = await basicForm.locator('button').textContent()
+    expect (buttonText).toEqual('Submit')
+
+    //all text values
+    const allRadioButtonText = await page.locator('nb-radio').allTextContents()
+    expect(allRadioButtonText).toContain('Option 1')
+
+    //input value
+    const emailField = basicForm.getByRole('textbox', {name: "Email"})
+    await emailField.fill('test@test.com')
+    const emailValue = await emailField.inputValue()
+    expect(emailValue).toEqual('test@test.com')
+
+    //attribute value
+    const placeholderValue = await emailField.getAttribute('placeholder')
+    expect(placeholderValue).toEqual(('Email'))
+})
