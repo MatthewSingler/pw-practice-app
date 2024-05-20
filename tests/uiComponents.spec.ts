a9e7077037d7c540f6bc24ed0b9faa2968644757
MatthewSingler <matthew.singler@gmail.com>
2024-05-20 13:13:20 -0500

    Radio buttons lesson

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -24,4 +24,24 @@ test.describe('Form Layouts page', () => {  //create the test suite
        //locator assertion
        await expect(usingTheGridEmailinput).toHaveValue('testtwo@test.com') //toHaveText will not work here so we use toHaveValue
     })
+
+    test('radio butons', async ({page}) => {
+        const usingTheGridForm = page.locator('nb-card', {hasText: "Using the Grid"})
+
+        //await usingTheGridForm.getByLabel('Option 1').check({force: true})
+        await usingTheGridForm.getByRole('radio', {name: "Option 1"}).check({force: true})
+
+        //generic assertion
+        const radioStatus = await usingTheGridForm.getByRole('radio', {name: "Option 1"}).isChecked()
+        expect(radioStatus).toBeTruthy()
+        //locator assertion
+        await expect(usingTheGridForm.getByRole('radio', {name: "Option 1"})).toBeChecked()
+
+        await usingTheGridForm.getByRole('radio', {name: "Option 2"}).check({force: true})
+        expect(await usingTheGridForm.getByRole('radio', {name: "Option 1"})).toBeFalsy()
+        expect(await usingTheGridForm.getByRole('radio', {name: "Option 2"})).toBeTruthy()
+
+
+
+    })
 })
